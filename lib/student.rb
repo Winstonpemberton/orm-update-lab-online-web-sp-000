45a40a61f3c086bcb401f005084a1122430fc864
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-14 22:34:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,41 +12,51 @@ class Student
   end
 
   def self.create_table
-  sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade TEXT
-  )
-  SQL
-
-  DB[:conn].execute(sql)
-end
-
-def self.drop_table
-  sql = "DROP TABLE IF EXISTS students"
-  DB[:conn].execute(sql)
-end
-
-def save
-  if self.id
-    self.update
-  else
     sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
     SQL
 
-      DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+        DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-end
+  end
 
-
-
+  def self.create(name:,grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
 
 end
