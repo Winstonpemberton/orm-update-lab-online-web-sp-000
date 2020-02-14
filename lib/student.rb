1d9578bd5f564155e390e25c4683a4969a316766
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-14 22:25:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,11 +6,11 @@ class Student
 
   def initialize(name, grade, id = nil)
     @name = name
-    @grade = grade 
+    @grade = grade
     @id = id
   end
 
-  
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
